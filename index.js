debd159b6e13753dad205764bda002a7d9cad344
Chris Cullen <chriscullen42@gmail.com>
2020-10-29 16:37:24 +0000

    initial tests complete and buldled with pkg tested

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ program.version('0.0.1')
 program
   .command('dump <source>')
   .description('Create a hex dump of the image file')
-  .option('-f, --format', 'output format')
+  .option('-f, --format <fmt>' , 'output format')
   .option('-x, --resize-x <width>', 'set the width')
   .option('-y, --resize-y <height>', 'set the height')
-  .action((source) => {
+  .action((source, cmdObj) => {
+    console.log("Running the dump command")
     doIt(source, cmdObj)
       .then(() => {
         console.log("done")
@@ -29,7 +30,13 @@ async function doIt(imageFile, cmdObj) {
   }
   // process the file
   let targetFile = path.join(path.dirname(imageFile), path.basename(imageFile, path.extname(imageFile)) + '.txt')
+  console.log(`Processing ${imageFile}\nInto ${targetFile}`)
   await dumpImage(image, imageFile, targetFile, 1, cmdObj.format)
 }
+
+console.log("*******************")
+console.log("IMAGE HEX DUMP UTILITY")
+console.log("*******************")
+program.parse(process.argv)
 
 
