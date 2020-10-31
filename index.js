a4aba0a648285197b51e3b803a37959063b8f6a3
Chris Cullen <chriscullen42@gmail.com>
2020-10-31 10:25:04 +0000

    Added repeated value removal and count of removed.

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ program.version('0.0.1')
 program
   .command('dump <source>')
   .description('Create a hex dump of the image file')
-  .option('-f, --format <fmt>' , 'output format')
+  .option('-f, --format <fmt>', 'output format')
   .option('-x, --width <width>', 'set the width')
   .option('-y, --height <height>', 'set the height')
+  .option('-u, --unique', 'Remove duplicated pixel values')
+  .option('-c, --count', 'If unique, show count of remove pixels')
   .action((source, cmdObj) => {
     console.log("Running the dump command")
     doIt(source, cmdObj)
@@ -24,9 +26,9 @@ async function doIt(imageFile, cmdObj) {
   const loadImage = require("./lib/loadImage")
   const dumpImage = require("./lib/dump")
   let resize = {}
-  if(cmdObj.width || cmdObj.height) {
-    if(cmdObj.width) resize.width = cmdObj.width *1
-    if(cmdObj.height) resize.height = cmdObj.height *1
+  if (cmdObj.width || cmdObj.height) {
+    if (cmdObj.width) resize.width = cmdObj.width * 1
+    if (cmdObj.height) resize.height = cmdObj.height * 1
   }
   let image = await loadImage(imageFile, resize)
   if (image instanceof Error) {
@@ -36,7 +38,7 @@ async function doIt(imageFile, cmdObj) {
   // process the file
   let targetFile = path.join(path.dirname(imageFile), path.basename(imageFile, path.extname(imageFile)) + '.txt')
   console.log(`Processing ${imageFile}\nInto ${targetFile}`)
-  await dumpImage(image, imageFile, targetFile, 1, cmdObj.format)
+  await dumpImage(image, imageFile, targetFile, 1, cmdObj.format, cmdObj.unique, cmdObj.count)
 }
 
 console.log("*******************")
